6f2a57ac98de393246800a407aea85c45c82c533
Steven J. Foland <stevenfoland@tsogiants.org>
2019-07-17 19:17:34 -0500

    End of Episode 05

--- Mechanical/Scripts/StepperGear.py
+++ Mechanical/Scripts/StepperGear.py
@@ -2,19 +2,17 @@ Units.Current = UnitTypes.Millimeters
 
 ### Parameters to Change ###
 PressureAngle = 20
-Thickness = 3
-DiametralPitch = 3
-Teeth = 24
+Thickness = 8
+PitchDiameter = 30
+Teeth = 15
 OutputFolder = 'C:\Users\steve\Documents\GitHub\Kepler\Work In Progress\Laser-CT-Scanner\Mechanical\CAD/'
-Name = "TestGear"
+Name = "StepperGear"
 ############################# 
 
 
 Gear = Part(Name)
-Circ = Gear.AddCircle(0,0,50,False)
-Gear.AddExtrudeBoss('Circle', Circ, Thickness, False)
-Profile = Gear.AddGearDN('Profile', DiametralPitch, Teeth, PressureAngle, 0, 0, Gear.GetPlane('XY-Plane'))
-Gear.AddExtrudeCut('Gear', Profile, Thickness, False)
+Profile = Gear.AddGearNP('Profile', Teeth, PitchDiameter, PressureAngle, 0, 0, Gear.GetPlane('XY-Plane'))
+Gear.AddExtrudeBoss('Gear', Profile, Thickness, False)
 print '%s Pitch Diameter = %f' % (Name, Profile.PitchDiameter)
 Gear.Save(OutputFolder)
 Gear.Close()
